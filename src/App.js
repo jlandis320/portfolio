a6196ec9cd1c6eff82e75314a3f55616047cac98
jlandis320 <jreynoldslandis@gmail.com>
2022-10-22 10:51:11 -0400

    sets up file structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import mac from './Macintosh-working.svg'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={mac} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
