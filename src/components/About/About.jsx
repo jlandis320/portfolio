6b54ce3d12b705fba10d6db1e679f414666a5621
jlandis320 <jreynoldslandis@gmail.com>
2022-11-13 15:30:56 -0500

    adds about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,10 +3,10 @@ import union from "./Union.svg"
 import styles from "./About.module.css"
 const About = () => {
   return ( 
-    <div id="about">
-      <img src={union} alt="placeholder img" className={styles.union} id="about"/>
+    <div id="about" style={{display: 'flex', flexDirection:'column', alignItems: 'center'}}>
+      <img src={union} alt="placeholder img" className={styles.union} />
       <h1>About Me</h1>
-      <p>brand statement</p>
+      <p className={styles.aboutp}>I'm a software engineer with an eye for design who loves finding the balance between flash and function. I take pride in creating well-crafted, user-oriented code. I bring creative problem solving and clear communication to every project.</p>
     </div>
   );
 }
