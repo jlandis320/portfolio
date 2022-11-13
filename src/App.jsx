631a15aa4486026ec09980f6ffdb97372007eae4
jlandis320 <jreynoldslandis@gmail.com>
2022-11-13 10:44:05 -0500

    adds netlify contact form

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 
-import Resume from "./pages/Resume/Resume";
+// import Resume from "./pages/Resume/Resume";
 import NavBar from "./components/NavBar/NavBar";
 import Main from "./pages/Main/Main";
 import { projects } from "./data/projects.jsx";
@@ -12,7 +12,7 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="/" element={<Main projects={projects} />} />
-        <Route path="/resume" element={<Resume />} />
+        {/* <Route path="/resume" element={<Resume />} /> */}
       </Routes>
     </div>
   );
