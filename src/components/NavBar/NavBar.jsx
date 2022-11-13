b572ca17241921473c0fef3193aa8f1b1e6c8e50
jlandis320 <jreynoldslandis@gmail.com>
2022-11-13 10:31:32 -0500

    adds resume download

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,15 +1,29 @@
 import { NavLink } from "react-router-dom";
+// import resume from 'Jess-Landis-Resume.pdf'
 
 import styles from './NavBar.module.css'
 
 const NavBar = () => {
+
+  const downloadRes = () => {
+    fetch('Jess-Landis-Resume.pdf').then(response => {
+      response.blob().then(blob => {
+        const fileURL = window.URL.createObjectURL(blob)
+        let alink = document.createElement('a')
+        alink.href = fileURL
+        alink.download = 'JessResume.pdf'
+        alink.click()
+      })
+    })
+  }
+
   return (
     <nav className={styles.bar} id="nav">
       <NavLink to='/' className={styles.navlink}>Home</NavLink>
       <a href="#about" className={styles.navlink}>About</a>
       <a href="#work" className={styles.navlink}>Work</a>
       <a href="#contact" className={styles.navlink}>Contact</a>
-      <NavLink to="/resume" className={styles.navlink}>Resumé</NavLink>
+      <button onClick={downloadRes} className={styles.navlink}>Resumé</button>
     </nav>
   );
 }
