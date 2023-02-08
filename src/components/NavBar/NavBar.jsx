635b478d6628f338488d2a47663ff9dccc3d74bc
jlandis320 <jreynoldslandis@gmail.com>
2023-02-08 16:55:02 -0500

    adds responsive nav

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,20 +2,9 @@
 // import resume from 'Jess-Landis-Resume.pdf'
 
 import styles from './NavBar.module.css'
+import PositionedMenu from '../PositionedMenu/PositionedMenu';
 
 const NavBar = () => {
-
-  // const downloadRes = () => {
-  //   fetch('Jess-Landis-Resume.pdf').then(response => {
-  //     response.blob().then(blob => {
-  //       const fileURL = window.URL.createObjectURL(blob)
-  //       let alink = document.createElement('a')
-  //       alink.href = fileURL
-  //       alink.download = 'JessLandisResume.pdf'
-  //       alink.click()
-  //     })
-  //   })
-  // }
 
   return (
     <nav className={styles.bar} id="nav">
@@ -24,6 +13,9 @@ const NavBar = () => {
       <a href="#work" className={styles.navlink} id="workbutton">Work</a>
       <a href="#contact" className={styles.navlink} id="contactbutton">Contact</a>
       <a href="https://jesslandis.netlify.app/Jess-Landis-Resume.pdf" className={styles.navlink} target="_blank" rel="noreferrer" id="resumebutton">Resumé</a>
+      <div className={styles.hamburger}>
+        <PositionedMenu />
+      </div>
     </nav>
   );
 }
---
+++ src/components/PositionedMenu/PositionedMenu.jsx
@@ -0,0 +1,50 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import MenuIcon from '@mui/icons-material/Menu';
+
+export default function PositionedMenu() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <Button
+        id="demo-positioned-button"
+        aria-controls={open ? 'demo-positioned-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
+      >
+        <MenuIcon style={{ fill: '#000000'}}/>
+      </Button>
+      <Menu
+        id="demo-positioned-menu"
+        aria-labelledby="demo-positioned-button"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'left',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'left',
+        }}
+      >
+        <MenuItem onClick={handleClose}><a href="#about">About</a></MenuItem>
+        <MenuItem onClick={handleClose}><a href="#work">Work</a></MenuItem>
+        <MenuItem onClick={handleClose}><a href="#contact">Contact</a></MenuItem>
+        <MenuItem onClick={handleClose}><a href="https://jesslandis.netlify.app/Jess-Landis-Resume.pdf" target="_blank" rel="noreferrer">Resumé</a></MenuItem>
+      </Menu>
+    </div>
+  );
+}
